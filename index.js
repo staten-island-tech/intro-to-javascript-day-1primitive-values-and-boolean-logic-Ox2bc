25995260ca4801af623638d870ece7ddbb7d0a11
Ox2bc <45404918+Ox2bc@users.noreply.github.com>
2020-03-25 10:21:30 -0400

    did assignment

---
+++ index.js
@@ -0,0 +1,71 @@
+/* let string = "strings";
+let sentance = "hello" + "Junshen";
+let sentance2 = " and Ema";
+//console.log(sentance + sentance2);
+let student1 = "Sandra";
+let student2 = "Kelly";
+let courseName = "CompSci";
+let teacherName = "Whalen"
+const longSentance = `${student1} and ${student2} are amazing and will be taking ${courseName} with ${teacherName}`
+ */
+/* let year = 2020;
+let nextYear = 2020+1;
+let past = 2020/2;
+let farFuture = 2020*50 */
+/* let trueStatement = true;
+let falseStatement = false;
+ */
+/* let ron;
+console.log(Sarah); */
+/* let student = "Caroline";
+let age = 15; */
+//console.log(student === "Caroline")
+//console.log(student === "caroline")
+/* console.log(age === 15);
+console.log(age === "15"); */
+/* console.log(age <= 15 || age > 19)
+console.log(age <= 15 && age > 19)
+ */
+/* let school = "iddle";
+if(school === "Elementary"){
+    console.log("you are in k-5")
+} else if (school === "middle"){
+    console.log("you are in 6th, 7th, or 8th grade")
+} else {
+    console.log("you are in HS")
+}
+*/
+
+//Formative Assesment #1
+let light = "green";
+if (light === "green"){
+    console.log("Go");
+} else if (light === "yellow"){
+    console.log("Caution");
+} else if (light === "red"){
+    console.log("Stop");
+}
+
+//Formative Assesment #2
+let Kristy = "lactoseIntolerant";
+let BaoBao = "milk";
+if (BaoBao === "milk" && Kristy === "milk"){
+    console.log("Get Milk");
+} else if (BaoBao === "lactoseIntolerant" && Kristy === "lactoseIntolerant"){
+    console.log("Get Almond Milk");
+} else {
+    console.log("Get Almond Milk and Normal Milk");
+}
+
+//Cupcake Challenge
+let temp = 69;
+if (temp < 68) {
+    console.log("It is too cold");
+} else if (temp >= 68 && temp <= 72){
+    console.log("It is a just perfect temperature");
+} else if (temp >= 73 && temp <= 76){
+    console.log("It is warm");
+} else {
+    console.log("It is too hot");
+}
+
